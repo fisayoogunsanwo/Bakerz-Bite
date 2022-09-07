76f98b1d455447c7c68a77c4a95dfa138c1b77b7
Fisayo Ogunsanwo <90068822+fisayoogunsanwo@users.noreply.github.com>
2022-09-07 15:17:40 +0100

    Cleaned up files

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let container = $(".container"); //the whole page
 
 
 // Start Page with loader
-document.addEventListener('DOMContentLoaded', function() {  setTimeout(() => {
-    loadDiv.css("display", "none");
-    container.css("display", "block");
-}, 1700)}, false);
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(() => {
+        loadDiv.css("display", "none");
+        container.css("display", "block");
+    }, 1700)
+}, false);
