03bc15f27b8ba2e0b3e3e2de0234ed060392a47f
Fisayo Ogunsanwo <90068822+fisayoogunsanwo@users.noreply.github.com>
2022-09-07 14:45:05 +0100

    Cleaned up files

--- popup.js
+++ popup.js
@@ -3,11 +3,9 @@ const closeModal = $(".close-modal");
 const modal = $(".outside-modal");
 
 openModal.on('click', () =>{
-    // openModal.classList("transi")
     modal.addClass("modal-display");
     modal.show();
 })
 closeModal.on('click', () =>{
-    // openModal.classList("transi");
     modal.hide();
 })
--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-let jsonDiv = $(".json-line");
 let loadDiv = $(".loader");
 let container = $(".container"); //the whole page
-let jsonURL = "./datastore.json"; //JSON file path
-let btn = $(".js-button");
-let sectionName = $(".section-name h2")
-let item = $(".item")
-let item1Img = $(".item1 img")
+
 
 
 // Start Page with loader
@@ -13,46 +8,3 @@ document.addEventListener('DOMContentLoaded', function() {  setTimeout(() => {
     loadDiv.css("display", "none");
     container.css("display", "block");
 }, 1700)}, false);
-
-
-// JSON Tests
-// let parsedContent = JSON.parse(content);
-// console.log(parsedContent.cookies[0].name);
-
-// btn.click(() => {
-//     getData();
-//     // jsonDiv.html= obj;
-// })
-
-// function getData(){
-//     fetch(jsonURL)
-//     .then(response => response.json())
-//     .then(data => {outputData (data)
-//     })
-// }
-
-// function outputData(obj){
-//     console.log(obj[1].pastries[1].name);
-
-//     let html = '';
-//     obj.forEach(() => {
-//         // console.log(obj[1].pastries[1].name);
-
-//         html+= `<div class="item1"><img src=${obj[1].pastries[1].name} alt="">
-//         <h4 class="product-name">Cookie Crumble</h4>
-//         <h3 class="product-price">$1000.50</h3>
-//         <button class="add-to-cart-btn">ADD TO CART</button>
-//     </div>`
-//     });
-//     item.html(html);
-//     ind++;
-// }
-
-// cakeBtn.click(()=>{
-//     sectionName.html("CAKES");
-//     console.log("cakes clicked");
-//     getData();
-// })
-
-
-
